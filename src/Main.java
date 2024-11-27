bdfaf99261fbe54ddf3f6e1a9c8b75d441d2e8be
Genesisvinan <jdencalahuaya@gmail.com>
2024-11-27 09:23:01 -0500

    Add(II): Add Draw Square method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import figures.Line;
+import figures.Square;
 
 import java.util.Scanner;
 
@@ -6,21 +7,21 @@ public class Main {
     public static void main(String[] args) {
         int option = 0;
         Scanner scanner = new Scanner(System.in);
-        Line line = new Line();
+        Square square= new Square();
         do {
             System.out.println("---- Figures System ----");
-            System.out.println("1. Create Line");
-            System.out.println("2. Print Line");
+            System.out.println("1. Create Square");
+            System.out.println("2. Print Square");
             System.out.println("3. Exit");
             option = scanner.nextInt();
             switch (option) {
                 case 1:
                     System.out.println("Please enter line length: ");
-                    int lineLength = scanner.nextInt();
-                    line.setLength(lineLength);
+                    int Length = scanner.nextInt();
+                    square.setLength(Length);
                     break;
                 case 2:
-                    line.drawLine();
+                    square.drawSquare();
                     break;
                 case 3:
                     System.out.println("--------- Exiting System ---------");
--- src/figures/Square.java
+++ src/figures/Square.java
@@ -10,10 +10,10 @@ public class Square {
         this.length = length;
     }
 
-    public void printProperties(){
+    public void drawSquare(){
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
