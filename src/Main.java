d8ef45dae624cca17e9340ce2a89bdd3509e4c5b
Genesisvinan <gvinan@est.ups.edu.ec>
2024-11-28 13:06:00 -0500

    Add(I): Square_Drawing_and_calculate

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,39 @@ public class Main {
     public static void main(String[] args) {
         int option = 0;
         Scanner scanner = new Scanner(System.in);
+        Line line = new Line();
         Square square= new Square();
         do {
             System.out.println("---- Figures System ----");
-            System.out.println("1. Create Square");
-            System.out.println("2. Print Square");
-            System.out.println("3. Exit");
+            System.out.println("1. Create line");
+            System.out.println("2. Print line");
+            System.out.println("3. Create Square");
+            System.out.println("4. Print Square");
+            System.out.println("5. Exit");
             option = scanner.nextInt();
             switch (option) {
                 case 1:
                     System.out.println("Please enter line length: ");
-                    int Length = scanner.nextInt();
-                    square.setLength(Length);
-                    break;
+                    int lineLength = scanner.nextInt();
+                    line.setLength(lineLength);
                 case 2:
-                    square.drawSquare();
+                    line.drawLine();
                     break;
                 case 3:
+                    System.out.println("Please enter Square length: ");
+                    int SquareLength = scanner.nextInt();
+                    square.setLength(SquareLength);
+                    break;
+                case 4:
+                    square.PrintSquare();
+                    break;
+                case 5:
                     System.out.println("--------- Exiting System ---------");
                     break;
                 default:
                     System.out.println("Please enter a valid option");
                     break;
             }
-        }while(option != 3);
+        }while(option != 5);
     }
 }
